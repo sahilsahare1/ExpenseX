29e5ec2ad3654983a37f0070a8fde927156eacb7
sahilsahare1 <saharesahil54@gmail.com>
2026-07-15 00:28:02 +0530

    Create login and home screen placceholder

--- app/src/main/java/com/sahil/expensex/presentation/auth/LoginScreen.kt
+++ app/src/main/java/com/sahil/expensex/presentation/auth/LoginScreen.kt
@@ -1,2 +1,28 @@
 package com.sahil.expensex.presentation.auth
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Login Screen",
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+    }
+
+}

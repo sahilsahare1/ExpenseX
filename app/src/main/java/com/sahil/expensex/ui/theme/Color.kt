30dd541464ebaa8193a548da3d33b6a366f7ace5
sahilsahare1 <saharesahil54@gmail.com>
2026-07-14 23:54:03 +0530

    Customize Material 3 color theme

--- app/src/main/java/com/sahil/expensex/ui/theme/Color.kt
+++ app/src/main/java/com/sahil/expensex/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.sahil.expensex.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF10B981)
+val Secondary = Color(0xFF3B82F6)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Background = Color(0xFFF8FAFC)
+val Surface = Color(0xFFFFFFFF)
+
+val ExpenseRed = Color(0xFFEF4444)
+val IncomeBlue = Color(0xFF2563EB)
+
+val TextPrimary = Color(0xFF1E293B)
+val TextSecondary = Color(0xFF64748B)

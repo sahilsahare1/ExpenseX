36e36cd08baef4f259e773d564e6ee60e0604422
sahilsahare1 <saharesahil54@gmail.com>
2026-07-20 19:03:53 +0530

    Refined Donut Chart

--- app/src/main/java/com/sahil/expensex/presentation/home/components/DonutChart.kt
+++ app/src/main/java/com/sahil/expensex/presentation/home/components/DonutChart.kt
@@ -1,14 +1,22 @@
 package com.sahil.expensex.presentation.home.components
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sahil.expensex.presentation.home.model.ExpenseCategory
 
 @Composable
@@ -21,9 +29,34 @@ fun DonutChart(
         modifier = Modifier.size(180.dp),
         contentAlignment = Alignment.Center
     ){
+
+        Column(
+            modifier=Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "₹14,250",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold
+
+            )
+
+            Text(
+                text = "Spent",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Gray
+
+            )
+
+        }
+
         Canvas(
             modifier = Modifier.size(180.dp)
             ) {
+
+
 
             var startAngle = -90f
 
@@ -35,7 +68,7 @@ fun DonutChart(
                     startAngle=startAngle,
                     sweepAngle=sweepAngle,
                     useCenter = false,
-                    style = Stroke(width = 72f),
+                    style = Stroke(width = 72f, cap = StrokeCap.Round),
 
 
                 )
